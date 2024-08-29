ab70e9df417550dd301308292800994b61c5831a
Christine <132790780+aesteri@users.noreply.github.com>
2024-08-30 07:18:24 +0900

    added revisions

--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
 
 { stdenv, cmake, mybar }:
 
-#make Derivation
+#make Derivation example
 stdenv.mkDerivation {
   pname = "target";
   version = "0.1.0";
   src = ./.;
 
-  #make install
+  #calls make install
   nativeBuildInputs = [ cmake ]; 
+
+  #other inputs you want while building this derivation
   buildInputs = [mybar];
 
 } 
